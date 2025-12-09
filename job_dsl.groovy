504fd8a1a9e8b31c25508640e62bc1fdf0e8fc52
Anahita Dormishian <anahita.dormishian@epitech.eu>
2025-12-09 16:17:41 +0100

    update my marvin

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,47 +1,12 @@
-def gitHubName = binding.variables.containsKey('GITHUB_NAME') ? GITHUB_NAME : null
-def displayName = binding.variables.containsKey('DISPLAY_NAME') ? DISPLAY_NAME : null
-
-folder('Tools') {
-    description('Folder for miscellaneous tools.')
-}
-
-job('Tools/clone-repository') {
-    description('Clone a repository from the provided URL')
-
-    parameters {
-        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
-    }
-
-    wrappers {
-        preBuildCleanup()
-    }
-
-    steps {
-        shell('git clone "$GIT_REPOSITORY_URL"')
-    }
-}
-
-job('Tools/SEED') {
-    description('Generate jobs from the provided GitHub repository and display name')
-
-    parameters {
-        stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name')
-        stringParam('DISPLAY_NAME', '', 'Display name for the job')
-    }
-
-    steps {
-        shell('cp /var/jenkins_home/job_dsl.groovy job_dsl.groovy')
-        dsl {
-            external('job_dsl.groovy')
-            lookupStrategy('SEED_JOB')
-        }
-    }
-}
+def gitHubName = binding.variables.get('GITHUB_NAME')
+def displayName = binding.variables.get('DISPLAY_NAME')
 
 if (gitHubName && displayName) {
+
     def repoUrl = "https://github.com/${gitHubName}"
 
-    job("${displayName}") {
+    job(displayName) {
+
         properties {
             githubProjectUrl(repoUrl)
         }
