24261912f3c40ed6bbd64efca09a6add4f9eeb84
Anahita Dormishian <anahita.dormishian@epitech.eu>
2025-12-10 12:44:49 +0100

    update my marvin

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,11 +1,72 @@
-def gitHubName = binding.variables.get('GITHUB_NAME')
-def displayName = binding.variables.get('DISPLAY_NAME')
+// Retrieve parameters when SEED job triggers this script
+def gitHubName = binding.variables.containsKey('GITHUB_NAME') ? GITHUB_NAME : null
+def displayName = binding.variables.containsKey('DISPLAY_NAME') ? DISPLAY_NAME : null
 
+// -----------------------------
+// Folder: Tools
+// -----------------------------
+folder('Tools') {
+    description('Folder for miscellaneous tools.')
+}
+
+// -----------------------------
+// Job: clone-repository
+// -----------------------------
+job('Tools/clone-repository') {
+    description('Clone a repository from the provided URL')
+
+    parameters {
+        stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
+    }
+
+    wrappers {
+        preBuildCleanup()
+    }
+
+    steps {
+        shell('git clone "$GIT_REPOSITORY_URL"')
+    }
+}
+
+// -----------------------------
+// Job: SEED (main generator job)
+// -----------------------------
+job('Tools/SEED') {
+    description('Generate jobs from the provided GitHub repository and display name')
+
+    parameters {
+        stringParam(
+            'GITHUB_NAME',
+            '',
+            'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")'
+        )
+        stringParam(
+            'DISPLAY_NAME',
+            '',
+            'Display name for the job'
+        )
+    }
+
+    steps {
+        // Copy DSL script into workspace
+        shell('cp /var/jenkins_home/job_dsl.groovy job_dsl.groovy')
+
+        // Execute DSL from workspace
+        dsl {
+            external('job_dsl.groovy')
+            lookupStrategy('SEED_JOB')
+        }
+    }
+}
+
+// -----------------------------
+// Generated job when parameters are provided
+// -----------------------------
 if (gitHubName && displayName) {
 
     def repoUrl = "https://github.com/${gitHubName}"
 
-    job(displayName) {
+    job("${displayName}") {
 
         properties {
             githubProjectUrl(repoUrl)
@@ -14,7 +75,7 @@ if (gitHubName && displayName) {
         scm {
             git {
                 remote {
-                    url("${repoUrl}.git")
+                    github(gitHubName, 'https')
                 }
                 branch("*/main")
             }
