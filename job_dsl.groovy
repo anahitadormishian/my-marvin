1672def6ad1ea91e0819bdb1125429eeb5def111
Aya0512 <aya.hasegawa@epitech.eu>
2025-12-11 13:28:41 +0100

    fix error for 19

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -16,7 +16,7 @@ job('Tools/SEED') {
 
     parameters {
         stringParam('GITHUB_NAME', '', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")' )
-        stringParam('DISPLAY_NAME', '', 'Display name for the generated job')
+        stringParam('DISPLAY_NAME', '', 'Display name for the job')
     }
 
     steps {
